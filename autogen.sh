533b29981e8aa3667514e016aae214bd8d369995
Christian Hammond <chipx86@chipx86.com>
2002-09-28 22:13:48 +0000

    [gaim-migrate @ 3662] Syncing autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-(./setup-gettext --gettext-tool) < /dev/null > /dev/null 2>&1 || {
+SETUP_GETTEXT=./setup-gettext
+
+($SETUP_GETTEXT --gettext-tool) < /dev/null > /dev/null 2>&1 || {
 	echo;
 	echo "You must have gettext installed to compile Gaim";
 	echo;
@@ -47,13 +49,24 @@ echo;
 cp -p po/ChangeLog po/ChangeLog.save
 
 echo "Running gettextize, please ignore non-fatal messages...."
-./setup-gettext
+$SETUP_GETTEXT
 
 # Restore the po/ChangeLog file.
 mv po/ChangeLog.save po/ChangeLog
 
 echo "Running libtoolize, please ignore non-fatal messages...."
 echo n | libtoolize --copy --force || exit;
+
+# Add other directories to this list if people continue to experience
+# brokennesses ...  Obviously the real answer is for them to fix it
+# themselves, but for Luke's sake we have this.
+for dir in "/usr/local/share/aclocal" \
+           "/opt/gnome-1.4/share/aclocal"
+do
+	if test -d $dir ; then
+		ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $dir"
+	fi
+done
 
 aclocal -I m4 $ACLOCAL_FLAGS || exit;
 autoheader || exit;
