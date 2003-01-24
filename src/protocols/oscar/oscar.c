91f32821faa8135ea7924e6435a1b9b40deb9d8a
Mark Doliner <mark@kingant.net>
2003-01-24 03:22:09 +0000

    [gaim-migrate @ 4681] This is what I meant to say, but, uh, there was a monkey on my shift and "1" keys...  Yeah...  A monkey...

--- src/protocols/oscar/oscar.c
+++ src/protocols/oscar/oscar.c
@@ -1321,7 +1321,7 @@ static int incomingim_chan1(aim_session_t *sess, aim_conn_t *conn, aim_userinfo_
 	if (args->icbmflags & AIM_IMFLAGS_AWAY)
 		flags |= IM_FLAG_AWAY;
 
-	if ((args->icbmflags & AIM_IMFLAGS_HASICON) && (!args->iconlen) && (!args->iconsum) && (!args->iconstamp)) {
+	if ((args->icbmflags & AIM_IMFLAGS_HASICON) && (args->iconlen) && (args->iconsum) && (args->iconstamp)) {
 		struct oscar_data *od = gc->proto_data;
 		struct icon_req *ir = NULL;
 		GSList *h = od->hasicons;
