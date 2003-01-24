11c63c4a10ae40f0f51b441d0681f3c259a8ec4d
Mark Doliner <mark@kingant.net>
2003-01-24 02:58:30 +0000

    [gaim-migrate @ 4678] I thought this was definately backportable.

--- src/protocols/oscar/oscar.c
+++ src/protocols/oscar/oscar.c
@@ -1321,15 +1321,12 @@ static int incomingim_chan1(aim_session_t *sess, aim_conn_t *conn, aim_userinfo_
 	if (args->icbmflags & AIM_IMFLAGS_AWAY)
 		flags |= IM_FLAG_AWAY;
 
-	if (args->icbmflags & AIM_IMFLAGS_HASICON) {
+	if ((args->icbmflags & AIM_IMFLAGS_HASICON) && (!args->iconlen) && (!args->iconsum) && (!args->iconstamp)) {
 		struct oscar_data *od = gc->proto_data;
 		struct icon_req *ir = NULL;
 		GSList *h = od->hasicons;
 		char *who = normalize(userinfo->sn);
 
-		if (!args->iconlen || !args->iconsum || !args->iconstamp)
-		    return 1;
-		    
 		debug_printf("%s has an icon\n", userinfo->sn);
 		while (h) {
 			ir = h->data;
