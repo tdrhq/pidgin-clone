d5a1bcd4f59ee6ef0a278419f5becbb03738a83b
Luke Schierer <lschiere@pidgin.im>
2004-09-17 03:16:33 +0000

    [gaim-migrate @ 10977] shx's previous patch needed fixing

--- src/protocols/msn/table.c
+++ src/protocols/msn/table.c
@@ -46,6 +46,7 @@ msn_table_new()
 	table->errors = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
 
 	table->async = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
+	table->fallback = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
 
 	return table;
 }
@@ -60,6 +61,7 @@ msn_table_destroy(MsnTable *table)
 	g_hash_table_destroy(table->errors);
 
 	g_hash_table_destroy(table->async);
+	g_hash_table_destroy(table->fallback);
 
 	g_free(table);
 }
