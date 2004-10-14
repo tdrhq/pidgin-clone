e0a5add12feafbe41582088bffe30491531223b6
Stu Tomlinson <stu@nosnilmot.com>
2004-10-14 01:49:46 +0000

    [gaim-migrate @ 11117] /me looks around looking for someone to blame. nope, no one to blame.
    Oh well, at least I can thank Damien Ayers for pointing out that this only
    got fixed on HEAD and not oldstatus.
    
    Thanks Damien!

--- src/protocols/msn/utils.c
+++ src/protocols/msn/utils.c
@@ -182,7 +182,7 @@ msn_import_html(const char *html, char **attributes, char **message)
 
 	memset(fontcolor, 0, sizeof(fontcolor));
 	strcat(fontcolor, "0");
-	memset(fonteffect, 0, sizeof(fontcolor));
+	memset(fonteffect, 0, sizeof(fonteffect));
 
 	for (c = html; *c != '\0';)
 	{
