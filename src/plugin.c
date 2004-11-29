2d5c100baa14c3a8d0e47221a547f869156a6db7
Ethan Blanton <elb@pidgin.im>
2004-11-29 13:17:06 +0000

    [gaim-migrate @ 11435] Nathan convinced me this is wrong, or could be.

--- src/plugin.c
+++ src/plugin.c
@@ -489,8 +489,6 @@ gaim_plugin_destroy(GaimPlugin *plugin)
 			plugin->info->major_version != GAIM_MAJOR_VERSION) {
 		if(plugin->handle)
 			g_module_close(plugin->handle);
-		if(plugin->path)
-			g_free(plugin->path);
 		g_free(plugin);
 		return;
 	}
