24d287614ddb1e452418fc47620bc28d3e26879b
Daniel Atallah <daniel.atallah@gmail.com>
2005-01-23 20:07:02 +0000

    [gaim-migrate @ 11898] Updated what was possible to do to the new API

--- plugins/gaim.pl
+++ plugins/gaim.pl
@@ -1,39 +1,56 @@
-sub description {
-        my($a, $b, $c, $d, $e, $f) = @_;
-        ("Example", "1.0", "An example Gaim perl script that does nothing particularly useful:\n\t-Show a dialog on load\n\t-Set user idle for 6,000 seconds\n\t-Greets people signing on with \"Hello\"\n\t-Informs you when script has been loaded for one minute.", "Eric Warmenhoven &lt;eric\@warmenhoven.org>", "http://gaim.sf.net", "/dev/null");
+use Gaim;
+
+%PLUGIN_INFO = (
+	perl_api_version => 2,
+	name             => "Example",
+	version          => "1.1",
+	summary          => "An example Gaim Perl plugin that does nothing useful.",
+	description      => "An example Gaim perl script that does nothing particularly useful:\n\t-Show a dialog on load (doesn't currently do this)\n\t-Set user idle for 6,000 seconds (also not currently possible in Perl API)\n\t-Greets people signing on with \"Hello\"\n\t-Informs you when script has been loaded for one minute. (currently in debug output)",
+	author           => "Eric Warmenhoven <eric\@warmenhoven.org>",
+	url              => "http://gaim.sf.net/",
+	load             => "plugin_load",
+	unload           => "plugin_unload"
+);
+
+sub plugin_init {
+	return %PLUGIN_INFO;
 }
 
-$handle = GAIM::register("Example", "1.0", "goodbye", "");
+sub plugin_load {
+	Gaim::debug_info("example perl plugin", "plugin_load\n");
+	my $plugin = shift;
 
-GAIM::print("Perl Says", "Handle $handle");
-		
-$ver = GAIM::get_info(0);
-@ids = GAIM::get_info(1);
+	# If the notification API had been implemented for Perl, it would be something like this to create a dialog
+	# Gaim::notify_info($plugin, "Example Perl Plugin", "Loaded");
 
-$msg = "Gaim $ver:";
-foreach $id (@ids) {
-	$pro = GAIM::get_info(7, $id);
-	$nam = GAIM::get_info(3, $id);
-	$msg .= "\n$nam using $pro";
+	# If there was a way to set the user Idle, it would be done here
+
+	# Listen for when buddies sign on so we can greet them
+	Gaim::signal_connect(Gaim::BuddyList::handle, "buddy-signed-on",
+		$plugin, \&buddy_signed_on_cb, "Hello!");
+
+	# Add a timer to prompt the user that the plugin has ben loaded for 60 seconds
+	Gaim::timeout_add($plugin, 60, \&notify);
 }
 
+sub plugin_unload {
+	my $plugin = shift;
+}
 
-GAIM::command("idle", 6000);
+sub buddy_signed_on_cb {
+	Gaim::debug_info("example perl plugin", "In buddy_signed_on_cb\n");
 
-GAIM::add_event_handler($handle, "event_buddy_signon", "echo_reply");
-GAIM::add_timeout_handler($handle, 60, "notify");
+	my ($buddy, $data) = @_;
 
-sub echo_reply {
-	$index = $_[0];
-	$who = $_[1];
-	GAIM::print_to_conv($index, $who, "Hello", 0);
+	Gaim::debug_info("example perl plugin", "Buddy '" .
+	                 $buddy->get_alias() . "' signed on.\n");
+
+	my $conv = Gaim::Conversation::IM::new($buddy->get_account(), $buddy->get_name());
+
+	$conv->send($data);
 }
 
 sub notify {
-	GAIM::print("1 minute", "gaim test has been loaded for 1 minute");
-}
-
-sub goodbye {
-	GAIM::print("You Bastard!", "You killed Kenny!");
+	Gaim::debug_info("example perl plugin", "gaim example perl plugin has been loaded for 1 minute\n");
 }
 
