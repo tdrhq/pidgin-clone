9978889756d562376d1606c909a0e8fbc75530c8
Daniel Atallah <daniel.atallah@gmail.com>
2005-02-11 03:50:41 +0000

    [gaim-migrate @ 11992] The glib G_GINT64_MODIFIER and friends are actually correct.  There is a bug in gcc < 3.4.1 that wasn't recognizing the native win32 format token

--- src/internal.h
+++ src/internal.h
@@ -134,16 +134,6 @@
 #	define g_open open
 #endif
 
-/* stupid, stupid, stupid */
-/* This is a workaround for the fact that G_GINT64_MODIFIER and
- * G_GUINT64_FORMAT are wrong, even in Glib >= 2.4 */
-#ifdef _WIN32
-#	undef  G_GINT64_MODIFIER
-#	define G_GINT64_MODIFIER "ll"
-#	undef  G_GUINT64_FORMAT
-#	define G_GUINT64_FORMAT "llu"
-#endif
-
 /* ugly ugly ugly */
 /* This is a workaround for the fact that G_GINT64_MODIFIER and G_GSIZE_FORMAT
  * are only defined in Glib >= 2.4 */
