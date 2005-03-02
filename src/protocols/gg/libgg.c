0fc3e85516413a1579264d872cb08b2cf19cd830
Stu Tomlinson <stu@nosnilmot.com>
2005-03-02 11:53:57 +0000

    [gaim-migrate @ 12165] This should fix Red Hat bug #149984, thanks to a helpful reporter who valgrinded for us.

--- src/protocols/gg/libgg.c
+++ src/protocols/gg/libgg.c
@@ -1,4 +1,4 @@
-/* $Id: libgg.c 10742 2004-08-25 03:09:08Z thekingant $ */
+/* $Id: libgg.c 12165 2005-03-02 11:53:57Z nosnilmot $ */
 
 /*
  *  (C) Copyright 2001 Wojtek Kaniewski <wojtekka@irc.pl>,
@@ -70,7 +70,7 @@ static char rcsid[]
 #ifdef __GNUC__
 __attribute__ ((unused))
 #endif
-= "$Id: libgg.c 10742 2004-08-25 03:09:08Z thekingant $";
+= "$Id: libgg.c 12165 2005-03-02 11:53:57Z nosnilmot $";
 
 #endif 
 
@@ -1055,7 +1055,7 @@ static int gg_watch_fd_connected(struct gg_session *sess, struct gg_event *e)
 
 		if (h->length >= sizeof(*s)) {
 			e->type = GG_EVENT_STATUS;
-			memcpy(&e->event.status, p, h->length);
+			memcpy(&e->event.status, p, sizeof(*s));
 			e->event.status.uin = fix32(e->event.status.uin);
 			e->event.status.status = fix32(e->event.status.status);
 		}
