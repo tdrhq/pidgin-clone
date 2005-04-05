6e7cb7d15b90a1df3db62073428035b761b32be0
Luke Schierer <lschiere@pidgin.im>
2005-04-05 11:36:34 +0000

    [gaim-migrate @ 12420] apparently my guess was wrong, this information was not available to my at release time

--- src/gtkdialogs.c
+++ src/gtkdialogs.c
@@ -97,7 +97,7 @@ static struct developer retired_developers[] = {
 };
 
 static struct translator current_translators[] = {
-	{N_("Bulgarian"),			"bg", "Alexander Shopov", "al_shopov@users.sf.net"},
+	{N_("Bulgarian"),			"bg", N_("Vladimira Girginova and Vladimir (Kaladan) Petkov", "missing@here.is,vpetkov@i-space.org"},
 	{N_("Catalan"),				"ca", "Josep Puigdemont", "tradgnome@softcatala.org"},
 	{N_("Czech"),				"cs", "Miloslav Trmac", "mitr@volny.cz"},
 	{N_("Danish"),				"da", "Morten Brix Pedersen", "morten@wtf.dk"},
