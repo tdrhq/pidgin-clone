ee83b7df8b9bfae4e40ea0e0cc610a0e14645976
Stu Tomlinson <stu@nosnilmot.com>
2005-05-15 13:27:59 +0000

    [gaim-migrate @ 12680] Fix the crash when renaming a group to an existing name

--- src/gtkblist.c
+++ src/gtkblist.c
@@ -3799,10 +3799,11 @@ static void gaim_gtk_blist_update_buddy(GaimBuddyList *list, GaimBlistNode *node
 
 	buddy = (GaimBuddy*)node;
 	contact = (GaimContact*)node->parent;
-	gtkparentnode = (struct _gaim_gtk_blist_node *)node->parent->ui_data;
 
 	/* First things first, update the contact */
 	gaim_gtk_blist_update_contact(list, node->parent);
+
+	gtkparentnode = (struct _gaim_gtk_blist_node *)node->parent->ui_data;
 
 	if(gtkparentnode->contact_expanded &&
 			(buddy->present != GAIM_BUDDY_OFFLINE ||
