66704f5748e1162ecb8cb16045b1a29f23fcfde7
Mark Doliner <mark@kingant.net>
2005-07-01 04:55:27 +0000

    [gaim-migrate @ 12975] Clean up the comments surrounding auto-tab switching in oldstatus (It's currently completely disabled, FYI)

--- src/conversation.c
+++ src/conversation.c
@@ -1533,8 +1533,17 @@ gaim_conversation_write(GaimConversation *conv, const char *who,
 	gaim_conversation_set_unseen(conv, unseen);
 
 	/*
-	 * If we received an IM, and the GaimConvWindow is not active,
-	 * then make this conversation the active tab in this GaimConvWindow.
+	 * TODO: This is #if 0'ed out because we don't have a way of
+	 *       telling if a conversation window is minimized.  This
+	 *       should probably be done in gtkconv.c anyway.
+	 */
+#if 0
+	/*
+	 * This is auto-tab switching.
+	 *
+	 * If we received an IM, and the GaimConvWindow is inactive and
+	 * minimized, then make this conversation the active tab in this
+	 * GaimConvWindow.
 	 *
 	 * We do this so that, when the user comes back to the conversation
 	 * window, the first thing they'll see is the new message.  This is
@@ -1544,14 +1553,12 @@ gaim_conversation_write(GaimConversation *conv, const char *who,
 	 */
 	if ((gaim_conversation_get_type(conv) == GAIM_CONV_IM) &&
 		(flags & (GAIM_MESSAGE_RECV | GAIM_MESSAGE_ERROR)) &&
-		(!gaim_conv_window_has_focus(win)))
+		(!gaim_conv_window_has_focus(win)) &&
+		(gaim_conv_window_is_minimized(win)))
 	{
-		/*
-		 * This is auto-tab switching.
-		 * This is currently not enabled because people are lame.  --Mark Doliner
-		 */
-		/* gaim_conv_window_switch_conversation(win, gaim_conversation_get_index(conv)); */
+		gaim_conv_window_switch_conversation(win, gaim_conversation_get_index(conv));
 	}
+#endif
 }
 
 void
