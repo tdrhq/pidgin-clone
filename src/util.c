5b13d7da0663d567869973433e010072878af138
Mark Doliner <mark@kingant.net>
2005-09-01 04:21:31 +0000

    [gaim-migrate @ 13643] sf patch #1252769, from Federico Schwindt Fix crashes due to non terminate strings

--- src/util.c
+++ src/util.c
@@ -2741,6 +2741,7 @@ url_fetched_cb(gpointer url_data, gint sock, GaimInputCondition cond)
 		return;
 	}
 
+	/* Read in data, one byte at a time */
 	if (read(sock, &data, 1) > 0 || errno == EWOULDBLOCK)
 	{
 		if (errno == EWOULDBLOCK)
@@ -2752,7 +2753,8 @@ url_fetched_cb(gpointer url_data, gint sock, GaimInputCondition cond)
 
 		gfud->len++;
 
-		if (gfud->len == gfud->data_len + 1)
+		/* If we've filled up our buffer then make it bigger */
+		if (gfud->len == gfud->data_len)
 		{
 			gfud->data_len += (gfud->data_len) / 2;
 
@@ -2760,6 +2762,7 @@ url_fetched_cb(gpointer url_data, gint sock, GaimInputCondition cond)
 		}
 
 		gfud->webdata[gfud->len - 1] = data;
+		gfud->webdata[gfud->len] = '\0';
 
 		if (!gfud->startsaving)
 		{
