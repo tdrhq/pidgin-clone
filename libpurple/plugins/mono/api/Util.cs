be9623a8975d901be9ab5459ab54c650db8b814b
Eoin Coffey <ecoffey@soc.pidgin.im>
2007-05-27 16:12:56 +0000

    Forgot Util.cs

---
+++ libpurple/plugins/mono/api/Util.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Purple {
+	class Util {
+		static public string build_string(IntPtr ptr)
+		{
+			int len = (int) (uint)ml_strlen(ptr);
+			byte[] bytes = new byte [len];
+			Marshal.Copy (ptr, bytes, 0, len);
+			return System.Text.Encoding.UTF8.GetString (bytes);
+		}
+		
+		[DllImport("libpurple")]
+		static extern int ml_strlen(IntPtr s);
+	}
+}
