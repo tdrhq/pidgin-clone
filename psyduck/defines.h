e54d63a10c21db2a2233a42a8f3f2d72b91a2ee9
Will Thompson <will.thompson@collabora.co.uk>
2007-06-30 13:48:35 +0000

    disapproval of revision '996e1d26d85852ff0da9778901dfaa5ef54f1b60'

--- psyduck/defines.h
+++
@@ -1,4 +0,0 @@
-#define CUSTOM_USER_DIRECTORY  "/dev/null"
-#define CUSTOM_PLUGIN_PATH     ""
-#define PLUGIN_SAVE_PREF       "/purple/psyduck/plugins/saved"
-#define UI_ID                  "psyduck"
