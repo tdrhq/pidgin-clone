913dc664f4e76cd08bfd7862feac2c80f7391bf4
Kevin Stange <kevin@simguy.net>
2007-07-16 01:02:47 +0000

    Initial checkin with partial home page recoding.

---
+++ htdocs/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+		"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>Home | Pidgin</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-language" content="en-US" />
+  <meta name="copyright" content="Pidgin Developers" />
+  <meta name="description" content="The home of Pidgin, Finch, and the libpurple IM client library." />
+  <meta name="keywords" content="pidgin, finch, libpurple, purple, IM, instant messaging, internet messaging, AIM, ICQ, XMMP, Jabber, MSN, Yahoo, Gadu-Gadu, IRC, Bonjour, SILC, SIP/SIMPLE, Zephyr, QQ, GroupWise, Sametime, Linux, Windows, GTK+" />
+</head>
+<body>
+
+<div id="header"><img src="/shared/img/logo.text.jpg" alt="Pidgin" />
+<ul>
+  <li class="active"><a href="/">Home</a></li>
+  <li><a href="/download/">Download</a></li>
+  <li><a href="/about/">About</a></li>
+  <li><a href="/news/">News</a></li>
+  <li><a href="http://developer.pidgin.im/">Support &amp; Development</a></li>
+</ul>
+</div>
+
+<div class="LeftHomeContent">
+<div class="LeftHomeContentTop">
+</div> <!-- .LeftHomeContentTop -->
+<div class="LeftHomeContentInner">
+
+<img class="BigPidginLogo" alt="Pidgin" src="/extension/pidgin/design/pidgin/images/pidgin_logo_big.gif"/>
+<span class="HomeFileVersion">
+2.0.2</span>
+
+<div class="HomeFileDownloadBlock">
+<div class="HomeFileDownloadBlockTop">
+</div>  <!-- .HomeFileDownloadBlockTop -->
+<div class="HomeFileDownloadBlockInner">
+<div class="Content">
+<a href="
+/download"><div class="DownloadButton"></div></a>
+<h3><a href="
+/download">Download Pidgin</a></h3>
+<a href="
+/download">
+2.0.2</a>
+</div>  <!-- .Content -->
+</div>  <!-- .HomeFileDownloadBlockInner -->
+<div class="HomeFileDownloadBlockBottom">
+</div>  <!-- .HomeFileDownloadBlockBottom -->
+</div>  <!-- .HomeFileDownloadBlock -->
+
+</div> <!-- .LeftHomeContentInner -->
+<div class="LeftHomeContentBottom">
+</div> <!-- .LeftHomeContentBottom -->
+
+<div class="BigPidgin">
+</div>
+
+<h3 class="HomeLeftText">
+</h3>
+</div> <!-- .LeftHomeContent -->
+
+
+
+
+<div class="RightHomeContent">
+<div class="RightHomeContentTop">
+</div> <!-- .RightHomeContentTop -->
+<div class="RightHomeContentInner">
+
+<h1>
+What is Pidgin?</h1>
+
+
+<p>Pidgin is a multi-protocol Instant Messaging client that allows you to use all of your IM accounts at once.</p>
+
+<p>Pidgin can work with:</p>
+
+<table class="default"  border="0" cellpadding="2" cellspacing="0" >
+<tr>
+<td valign="top">  
+<ul>
+
+<li>AIM</li>
+
+<li>Bonjour</li>
+
+<li>Gadu-Gadu</li>
+
+<li>Google Talk</li>
+
+<li>Groupwise</li>
+
+</ul>
+
+  </td>
+
+<td valign="top">  
+<ul>
+
+<li>ICQ</li>
+
+<li>IRC</li>
+
+<li>MSN</li>
+
+<li>QQ</li>
+
+<li>SILC</li>
+
+</ul>
+
+  </td>
+
+<td valign="top">  
+<ul>
+
+<li>SIMPLE</li>
+
+<li>Sametime</li>
+
+<li>XMPP</li>
+
+<li>Yahoo!</li>
+
+<li>Zephyr</li>
+
+</ul>
+
+  </td>
+</tr>
+
+</table>
+
+<div class="object-center embedCentered"><div class="content-view-embed">
+	<div class="class-image">
+		<div class="attribute-image">
+			<p>
+				    				
+
+    
+        
+    
+        
+    
+    
+    							</p>
+		</div>
+
+			</div>
+</div></div>
+<div class="object-right"><div class="content-view-embed">
+	<div class="class-image">
+		<div class="attribute-image">
+			<p>
+				    				
+
+    
+        
+    
+    <a href="/pidgin/about" target="_self">            <img src="/var/ezwebin_site/storage/images/media/images/learn_more_button/703-2-eng-US/learn_more_button.gif" width="116" height="29"  style="border: 0px;" alt="Learn More" title="Learn More" /></a>    
+    
+    
+    							</p>
+		</div>
+
+			</div>
+</div></div>
+</div> <!-- .RightHomeContentInner -->
+<div class="RightHomeContentBottom">
+</div> <!-- .RightHomeContentBottom -->
+</div> <!-- .RightHomeContent -->
+
+
+<div class="FooterInformation">
+
+<div class="object-center"><div class="content-view-embed">
+	<div class="class-image">
+		<div class="attribute-image">
+			<p>
+				    				
+
+    
+        
+    
+        
+    
+    
+    							</p>
+		</div>
+
+			</div>
+</div></div></div> <!-- .FooterInformation -->
+</div> <!-- #Container -->
+</div> <!-- #BodyWrapper -->
+</body>
+</html><!-- Generated: 2007-06-20 21:11:02 -->
+
