9f815b6484e2c33d687116690ff7c0a0116bc724
Kevin Stange <kevin@simguy.net>
2007-07-18 07:52:56 +0000

    Main download page done.  Next up: dynamic left menu / include, then news page!

---
+++ htdocs/download/index.php
@@ -0,0 +1,42 @@
+<?
+  // Update these to match the current page.
+  $page['title'] =   "Download";
+  $page['section'] = "Download";
+  $page['subsect'] = "";
+
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/header.inc");
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/version.inc");
+?>
+
+<div id="content">
+
+<div class="box_nav">
+<ul>
+  <li><a href="/download/windows/">Windows</a></li>
+  <li><a href="/download/source/">Source</a></li>
+  <li><a href="/download/fedora_core/">Fedora Core</a></li>
+  <li><a href="/download/centos_rhel/">CentOS / RHEL</a></li>
+</ul>
+</div>
+
+<div class="box_right">
+<div id="main">
+
+<h1>Download</h1>
+
+<p>Pidgin runs on all Windows and UNIX platforms. In addition to the source
+code, we distribute binaries for Windows, Fedora Core, and CentOS / RHEL.</p>
+
+<ul>
+  <li><a href="/download/windows/">Windows</a></li>
+  <li><a href="/download/source/">Source</a></li>
+  <li><a href="/download/fedora_core">Fedora Core</a></li>
+  <li><a href="/download/centos_rhel">CentOS / RHEL</a></li>
+</ul>
+
+</div>
+</div>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/../inc/footer.inc"); ?>
