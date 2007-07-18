6b8a4e641a7e0468e423440d13dc86247c5d795e
Kevin Stange <kevin@simguy.net>
2007-07-18 08:39:45 +0000

    News page is templated, but I am not sure how this functions yet...

---
+++ htdocs/news/index.php
@@ -0,0 +1,27 @@
+<?
+  // Update these to match the current page.
+  $page['title'] =   "News";
+  $page['section'] = "News";
+  $page['subsect'] = "";
+
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/header.inc");
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/version.inc");
+?>
+
+<div id="content">
+
+<? include($_SERVER['DOCUMENT_ROOT'] . "/../inc/section.news.inc"); ?>
+
+<div class="box_right">
+<div id="main">
+
+<h1>News</h1>
+
+<p>This page is coming soon.  Stay tuned!</p>
+
+</div>
+</div>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/../inc/footer.inc"); ?>
