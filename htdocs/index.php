cf8a57d4dc4706e4f66d5c57898cd3f95c92de38
Kevin Stange <kevin@simguy.net>
2007-07-18 06:50:10 +0000

    Separated out header, footer, version into includes.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,35 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-		"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-  <title>Home | Pidgin</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta http-equiv="Content-language" content="en-US" />
-  <meta name="copyright" content="Pidgin Developers" />
-  <meta name="description" content="The home of Pidgin, Finch, and the libpurple IM client library." />
-  <meta name="keywords" content="pidgin, finch, libpurple, purple, IM, instant messaging, internet messaging, AIM, ICQ, XMMP, Jabber, Google Talk, MSN, Yahoo, Gadu-Gadu, IRC, Bonjour, SILC, SIP/SIMPLE, Zephyr, QQ, GroupWise, Sametime, Linux, Windows, GTK+" />
-  <link rel="stylesheet" href="/shared/css/main.css" />
-</head>
-<body>
+<?
+  // Update these to match the current page.
+  $page['title'] =   "Home";
+  $page['section'] = "Home";
 
-<div id="header"><img src="/shared/img/logo.text.jpg" alt="Pidgin" />
-<ul>
-  <li class="active"><a href="/">Home</a></li>
-  <li><a href="/download/">Download</a></li>
-  <li><a href="/about/">About</a></li>
-  <li><a href="/news/">News</a></li>
-  <li><a href="http://developer.pidgin.im/">Support &amp; Development</a></li>
-</ul>
-</div>
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/header.inc");
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/version.inc");
+?>
 
 <div id="content">
 
 <div class="box_home">
-
 <div id="left">
 
 <div id="version"><img src="/shared/img/logo.text-big.gif" alt="Pidgin" />
-<span class="number">2.0.2</span>
+<span class="number"><? echo $pidgin_version; ?></span>
 </div>
 
 <div id="download">
@@ -37,15 +21,13 @@
 <a href="/download/">
 <img src="/shared/img/button.download.png" alt="[Down Arrow]" id="downbutton" />
 Download Pidgin<br />
-<span class="number">2.0.2</span></a>
+<span class="number"><? echo $pidgin_version; ?></span></a>
 </div>
 
 </div>
-
 </div>
 
 <div class="box_home">
-
 <div id="main">
 
 <h1>What is Pidgin?</h1>
@@ -75,16 +57,8 @@ Download Pidgin<br />
 <p class="more"><a href="/about/"><img src="/shared/img/button.learn-more.gif" alt="Learn More" /></a></p>
 
 </div>
-
 </div>
 
 </div>
 
-<div id="footer">
-<!-- Need a SF.net and Luke hosting banner here -->
-<a href="http://sourceforge.net"><IMG src="http://sourceforge.net/sflogo.php?group_id=235&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net" /></a>
-</div>
-
-</body>
-</html>
-
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/../inc/footer.inc"); ?>
