2eb2401ac7d5d418b3bdb4949532ae4a379e7662
Eoin Coffey <ecoffey@soc.pidgin.im>
2007-07-24 02:06:26 +0000

    Heres the flow I'm trying to model out in api/Signal.cs
    1. a .Net plugin hooks into an event, and Signal.connect eventually gets called with the signal name,
    and a delegate into the .Net plugin
    2. Signal.cs stores the provided delegate in a hash of signal names => vector of delegates. (Not Implemented)
    3. Signal.cs calls _connect and pass in its own delegate instead (Currently hardcoded since we only have one signal),
    and passes in the string signal name as the user_data
    4. When libpurple core fires the signal, Signal.cs's delegate gets called, uses the signal name provided in the
    user_data to look into the hash map, converts IntPtrs to Purple.Objects and calls all the real delegates. (Somewhat implemented)
    
    Basically all I'm doing with step 4 is passing in the string signal name and dumping it to the console, but its not
    getting passed around, which means I can't then fire off the real delegates.  Hmmm.

--- libpurple/plugins/mono/api/Signal.cs
+++ libpurple/plugins/mono/api/Signal.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Purple
@@ -7,14 +8,31 @@ namespace Purple
 	{
 		public delegate void Handler(IntPtr p);
 
-		public delegate void VOID__POINTER_POINTER_POINTER(IntPtr p1, IntPtr p2, IntPtr p3);
+		public delegate void VOID__POINTER_POINTER_POINTER_dl(IntPtr p1, IntPtr p2, IntPtr p3, IntPtr signal);
+
+		public static void VOID__POINTER_POINTER_POINTER(IntPtr p1, IntPtr p2, IntPtr p3, IntPtr signal)
+		{
+			Console.WriteLine("In signal callback: " + p1 + ", " + p2 + ", " + p3 + ", " + signal);
+		}
 
 		public static int connect(IntPtr instance, string signal, IntPtr handle, Delegate func, IntPtr data)
+		{
+			MethodInfo mi = func.GetType().GetMethod("Invoke", BindingFlags.Public | BindingFlags.Instance);
+			
+			foreach (ParameterInfo p in mi.GetParameters())
+			{
+				Console.WriteLine(p.ParameterType);
+			}
+
+			return _connect(instance, signal, handle, new VOID__POINTER_POINTER_POINTER_dl(VOID__POINTER_POINTER_POINTER), signal);
+		}
+
+		private static int _connect(IntPtr instance, string signal, IntPtr handle, Delegate func, string data)
 		{
 			return purple_signal_connect(instance, signal, handle, func, data);
 		}
 
 		[DllImport("libpurple")]
-		extern private static int purple_signal_connect(IntPtr instance, string signal, IntPtr handle, Delegate func, IntPtr data);
+		extern private static int purple_signal_connect(IntPtr instance, string signal, IntPtr handle, Delegate func, string data);
 	}
 }
