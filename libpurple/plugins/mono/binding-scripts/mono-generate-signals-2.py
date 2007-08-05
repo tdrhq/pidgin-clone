9348df6b4267e5dc17f13dc99e570ec2ad32ebe1
Eoin Coffey <ecoffey@soc.pidgin.im>
2007-08-05 03:51:38 +0000

    The generate script can now properly parse out a purple_signal_register.
    Just dumps the output for now in the form of signal_name -> return_type arg types

--- libpurple/plugins/mono/binding-scripts/mono-generate-signals-2.py
+++ libpurple/plugins/mono/binding-scripts/mono-generate-signals-2.py
@@ -2,62 +2,77 @@ import sys
 import re
 import string
 
-input = iter(sys.stdin)
 
-signal_reg = re.compile(".*purple_signal_register.*")
-end_call = re.compile(".*\);")
 
-buffer = ""
-parse_signal_reg = False
+def parse_signal_register_calls(input):
+	signal_reg = re.compile(".*purple_signal_register.*")
+	end_call = re.compile(".*\);")
 
-signal_regs = []
+	buffer = ""
+	parse_signal_reg = False
 
-for line in input:
-	if not parse_signal_reg:
-		if signal_reg.match(line):
-			buffer = line.strip()
+	signal_regs = []
+
+	for line in input:
+		if not parse_signal_reg:
+			if signal_reg.match(line):
+				buffer = line.strip()
+				if end_call.match(line):
+					signal_regs.append(buffer)
+					parse_signal_reg = False
+					continue
+				parse_signal_reg = True
+				continue
+
+		if parse_signal_reg:
+			buffer = buffer + line.strip()
 			if end_call.match(line):
 				signal_regs.append(buffer)
 				parse_signal_reg = False
 				continue
-			parse_signal_reg = True
-			continue
-
-	if parse_signal_reg:
-		buffer = buffer + line.strip()
-		if end_call.match(line):
-			signal_regs.append(buffer)
-			parse_signal_reg = False
-			continue
+	
+	return signal_regs
 
 
-func_parse = re.compile("purple_signal_register\(handle\s*,\s*\"([A-Za-z0-9_-]+)\"\s*,\s*([A-Za-z0-9_-]+)\s*,\s*[A-Za-z0-9_-]+\s*,\s*([0-9]+)\s*,?\s*(.*)\s*(\);)$");
+def parse_signal_register_details(signal_regs):
+	func_parse = re.compile("purple_signal_register\(handle\s*,\s*\"([A-Za-z0-9_-]+)\"\s*,\s*([A-Za-z0-9_-]+)\s*,\s*(.+)\s*,\s*([0-9]+)\s*,?\s*(.*)\s*(\);)$");
 
-for func in signal_regs:
-	print "func: " + func
+	return_type_parse = re.compile("(purple_value_new\(PURPLE_TYPE_SUBTYPE,|purple_value_new\()?\s*([A-Za-z_-]+)")
 
-	m = func_parse.match(func)
+	signals = {}
 
-	if m:
-		signal_name = m.group(1)
-		marshall = m.group(2)
-		number_args = int(m.group(3))
-		print "signal name: " + signal_name
-		print "marshall: " + marshall
-		print "number: " + str(number_args)
-		if number_args > 0:
-			values = m.group(4)
-			value_parse_str = "purple_value_new\((PURPLE_TYPE_SUBTYPE)?,?([A-Z_]+)\)\s*,?\s*" * number_args
+	for func in signal_regs:
+		m = func_parse.match(func)
+	
+		if m:
+			signal_name = m.group(1)
+			marshall = m.group(2)
+			return_type_str = m.group(3)
+			number_args = int(m.group(4))
 
-			value_parse = re.compile(value_parse_str)
+			return_type_m = return_type_parse.match(return_type_str)
 
-			v = value_parse.match(values)
+			signals[signal_name] = [return_type_m.group(2)]
 
-			if v:
-				for g in v.groups():
-					print g
-			print value_parse_str
+			#print "Parsing signal: " + signal_name
+
+			if number_args > 0:
+				values = m.group(5)
+	
+				value_parse = re.compile("purple_value_new\((PURPLE_TYPE_SUBTYPE|PURPLE_TYPE_BOXED)?,?\s*(.+)\)\s*,?\s*" * number_args)
+
+				v = value_parse.match(values)
+
+				if v:
+					for g in v.groups():
+						if g and g != "PURPLE_TYPE_SUBTYPE" and g != "PURPLE_TYPE_BOXED":
+							signals[signal_name].append(g)
+
+	return signals
 
 
-	print ""
+	
+input = iter(sys.stdin)
 
+for k, v in parse_signal_register_details(parse_signal_register_calls(input)).items():
+	print k, v
