684ae760896f9673f61a59dc93b34b071882154b
Kevin Stange <kevin@simguy.net>
2007-08-10 08:31:00 +0000

    This fixes rendering issues on IE 7.  Some custom javascript and CSS, conditional only to IE 7 users.

---
+++ htdocs/shared/js/ie7-hacks.js
@@ -0,0 +1,33 @@
+function search(element) {
+  // Get a list of divs in the document
+  var divs = element.getElementsByTagName("div");
+  for (var i = 0; divs[i] != null; i++) {
+    var type = null;
+    // If the divs are "boxes", boxify them (mimick :before/:after)
+    if (divs[i].className.match(/^box_[a-z]+[^_]$/)) {
+      type = divs[i].className.match(/^box_(.*)$/)[1];
+      type = "box_" + type;
+      boxify(divs[i], type);
+    // If the divs is the "download bubble," do the same.
+    } else if (divs[i].id == "download") {
+      boxify(divs[i], divs[i].id);
+    }
+  }
+}
+
+// Create a box before and after the element containing its borders
+function boxify(element, type) {
+  before = document.createElement("div");
+  before.className = type + "_before";
+  before.innerHTML = "&nbsp;";
+  element.insertBefore(before, element.firstChild);
+
+  after = document.createElement("div");
+  after.className = type + "_after";
+  after.innerHTML = "&nbsp;";
+  element.insertBefore(after);
+
+  return;
+}
+
+search(document);
