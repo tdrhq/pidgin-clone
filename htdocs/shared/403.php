8fe735eed17588531fd0706e2141dc138391da01
Kevin Stange <kevin@simguy.net>
2007-08-11 20:30:43 +0000

    Add 404 and 403 error pages.

---
+++ htdocs/shared/403.php
@@ -0,0 +1,27 @@
+<?
+  // Update these to match the current page.
+  $page['title'] =   "403 Forbidden";
+  $page['section'] = "";
+
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/header.inc");
+?>
+
+<div id="content">
+
+<div class="box_full">
+<div id="main">
+
+<h1>403 Forbidden</h1>
+
+<p>Sorry, the page you tried to reach is hidden!  Please try using other
+links on the site to find what you're looking for.  If you're sure this
+page should work, please submit a <a
+href="http://developer.pidgin.im/newticket">new ticket</a> to let us know
+about the problem.</p>
+
+</div>
+</div>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/../inc/footer.inc"); ?>
---
+++ htdocs/shared/404.php
@@ -0,0 +1,27 @@
+<?
+  // Update these to match the current page.
+  $page['title'] =   "404 Not Found";
+  $page['section'] = "";
+
+  include($_SERVER['DOCUMENT_ROOT'] . "/../inc/header.inc");
+?>
+
+<div id="content">
+
+<div class="box_full">
+<div id="main">
+
+<h1>404 Not Found</h1>
+
+<p>Sorry, the page you tried to reach isn't here!  Please try using other
+links on the site to find what you're looking for.  If you're sure this
+page should work, please submit a <a
+href="http://developer.pidgin.im/newticket">new ticket</a> to let us know
+about the problem.</p>
+
+</div>
+</div>
+
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/../inc/footer.inc"); ?>
