20441caf0cd7d437dd572b468fc8e5ee8ad12e6f
Eoin Coffey <ecoffey@soc.pidgin.im>
2007-08-18 19:01:43 +0000

    Working on getting mono-generate-module to generate properties for a class.

--- libpurple/plugins/mono/binding-scripts/mono-generate-module.py
+++ libpurple/plugins/mono/binding-scripts/mono-generate-module.py
@@ -21,6 +21,8 @@ class Buddy : Object {
 The script will have to handle:
 * Translation of C types to C# types (any pointer -> IntPtr)
 * Handle subclassing (i.e. Buddy should subclass from BListNode)
+
+TODO: Figure out a clean way to create C# Properties for a given class
 """
 
 import sys
@@ -36,13 +38,27 @@ def to_function_name(str):
 			out += c
 	return re.sub("^_", "", out)
 
+def convertname(name):
+	newname = ""
+	for w in name.split("-"):
+		newname += w.capitalize()
+	return newname
+
+def clean_arg(arg):
+	return re.sub("const", "", arg.strip()).strip()
+
+def is_purple_object(arg):
+	a = clean_arg(arg).split()
+	if len(a) != 2:
+		return False
+	return (re.match("^Purple", a[0]) and a[1].count('*') == 1)
+
 def determine_marshall_type(arg):
 	"""
 	Returns the type used to marshall between C# and C code (i.e. PurpleBuddy* -> IntPtr)
+	TODO: handle "primitive" types (i.e. gboolean -> bool)
 	"""
-	s = arg.strip()
-	s = re.sub("const", "", s).strip()
-	a = s.split()
+	a = clean_arg(arg).split()
 	if len(a) > 1:
 		if a[1].count('*') == 1:
 			return "IntPtr"
@@ -53,15 +69,44 @@ def determine_marshall_type(arg):
 def determine_csharp_type(arg):
 	"""
 	Returns the type used by the C# Purple API (i.e. PurpleBuddy* -> Buddy)
+	TODO: handle "primitive" types (i.e. gboolean -> bool)
 	"""
-	s = arg.strip()
-	s = re.sub("const", "", s).strip()
-	a = s.split()
-	if re.match("^Purple", a[0]):
+	a = clean_arg(arg).split()
+	if is_purple_object(arg):
 		return re.sub("^Purple", "", a[0])
 	if re.match("char", a[0]) and a[1].count('*') == 1:
 		return "string"
 	return a[0]
+
+class ArgType:
+	def __init__(self, cstr):
+		self.marshall = determine_marshall_type(cstr)
+		self.csharp = determine_csharp_type(cstr)
+		self.is_purple_object = is_purple_object(cstr) 
+
+class Property:
+	def __init__(self, propname):
+		self.propname = propname.strip('_')
+		self.cspropname = convertname(self.propname)
+
+		self.actions = {}
+
+	def add(self, operation, method):
+		self.actions[operation] = method
+
+	def dump(self):
+		property = """
+		public %s %s {
+			%s
+		}
+		"""
+
+		body = ""
+		if self.actions.has_key('get'):
+			meth = self.actions['get']
+			body += "get { "
+			body += "return "
+
 
 class Method:
 	def __init__(self, ret_str, func_str, arg_str_list):
@@ -69,16 +114,12 @@ class Method:
 		self.func_str = func_str
 		self.arg_str_list = arg_str_list
 
-		self.ret_marshall_type = determine_marshall_type(self.ret_str)
-		self.ret_csharp_type = determine_csharp_type(self.ret_str)
+		self.ret_type = ArgType(self.ret_str)
 
-		self.arg_marshall_type_list = []
-		self.arg_csharp_type_list = []
-
+		self.arg_type_list = []
 
 		for arg in self.arg_str_list:
-			self.arg_marshall_type_list.append(determine_marshall_type(arg))
-			self.arg_csharp_type_list.append(determine_csharp_type(arg))
+			self.arg_type_list.append(ArgType(arg))
 
 	
 	def as_property(self):
@@ -93,6 +134,8 @@ class Method:
 			body = "Util.build_string("
 		else:
 			body =  "return "
+			if self.ret_csharp_type == "string":
+				body += "Util.build_string(%s);" % (self.func_str)
 			body += ""
 
 		#elif self.r
@@ -103,12 +146,9 @@ class Method:
 		static private extern %s %s(%s);
 		"""
 
-		return dllimport % (self.ret_marshall_type, self.func_str, string.join(self.arg_marshall_type_list, ","))
+		return dllimport % (self.ret_type.marshall, self.func_str, string.join([s.marshall for s in self.arg_type_list], ","))
 
 	def __repr__(self):
-		s =  "Original:  " + self.ret_str + " : " + self.func_str + " : " + str(self.arg_str_list) + "\n"
-		s += "Marshall:  " + self.ret_marshall_type + " : " + self.func_str + " : " + str(self.arg_marshall_type_list) + "\n"
-		s += "PurpleAPI: " + self.ret_csharp_type + " : " + self.func_str + " : " + str(self.arg_csharp_type_list) + "\n\n"
 		return self.as_dllimport() 
 
 class Struct:
@@ -116,11 +156,13 @@ class Struct:
 		self.structname = structname
 		self.csname = re.sub("^Purple", "", structname)
 		func_name = to_function_name(structname)
-		self.func_nonstatic_parse_str = "([A-Za-z]+\s*\*?)\s*(%s_(get|set|new|on).*)\((.*)\);" % (func_name)
+		self.func_nonstatic_parse_str = "([A-Za-z]+\s*\*?)\s*(%s_(get|set|new|on)(.*))\((.*)\);" % (func_name)
 		self.func_parse_str = "([A-Za-z]+\s*\*?)\s*(%s[A-Za-z0-9_]+)\((.*)\);" % (func_name)
 		self.func_nonstatic_parse = re.compile(self.func_nonstatic_parse_str)
 
 		self.nonstatic_methods = []
+
+		self.properties = {}
 
 		print self.func_nonstatic_parse_str
 
@@ -131,8 +173,20 @@ class Struct:
 			return False
 
 		ret_type = match.group(1)
+		func_name = match.group(2)
+		operation = match.group(3)
+		prop_name = match.group(4)
+		args = match.group(5)
 
-		self.nonstatic_methods.append(Method(match.group(1), match.group(2), match.group(4).split(',')))
+		meth = Method(ret_type, func_name, args.split(','))
+
+		if operation != "new":
+			if not self.properties.has_key(prop_name):
+				self.properties[prop_name] = Property(prop_name)
+
+			self.properties[prop_name].add(operation, meth)
+
+		self.nonstatic_methods.append(meth)
 
 		return True
 
