332fe85df18a34ae5f9163a4e592bd145e65f9c1
Luke Schierer <lschiere@pidgin.im>
2007-10-24 15:58:38 +0000

    when can we publicize this?

--- htdocs/inc/version.inc.php
+++ htdocs/inc/version.inc.php
@@ -3,8 +3,8 @@
 	 * This variable will be inserted into the source tarball and bz2ball
 	 * path. Update with every new release.
 	 */
-	$current_version = "2.1.0";
-	$current_win32_version = "2.1.0";
+	$current_version = "2.2.2";
+	$current_win32_version = "2.2.2";
 	$current_win32_gtk_version = "2.10.13";
 	$current_win32_gtk_revision = "a";
 ?>
