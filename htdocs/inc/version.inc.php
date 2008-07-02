842ce357c7edcbdcd06cd3f6f5ee217a8359225c
Stu Tomlinson <stu@nosnilmot.com>
2008-07-02 03:51:45 +0000

    2.4.3

--- htdocs/inc/version.inc.php
+++ htdocs/inc/version.inc.php
@@ -3,8 +3,8 @@
 	 * This variable will be inserted into the source tarball and bz2ball
 	 * path. Update with every new release.
 	 */
-	$current_version = "2.4.2";
-	$current_win32_version = "2.4.2";
+	$current_version = "2.4.3";
+	$current_win32_version = "2.4.3";
 	$current_win32_gtk_version = "2.12.8";
 	$current_win32_gtk_revision = "a";
 ?>
