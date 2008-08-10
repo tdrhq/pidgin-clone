2f37262cedfb79127b1958165a0098fc26209679
Elliott Sales de Andrade <qulogic@pidgin.im>
2008-08-10 23:19:34 +0000

    Fix some gc->account to purple_connection_get_account(gc) problems.

--- libpurple/prpl.c
+++ libpurple/prpl.c
@@ -392,6 +392,7 @@ purple_prpl_get_statuses(PurpleAccount *account, PurplePresence *presence)
 void
 purple_prpl_send_attention(PurpleConnection *gc, const char *who, guint type_code)
 {
+	PurpleAccount *account;
 	PurpleAttentionType *attn;
 	PurpleMessageFlags flags;
 	PurplePlugin *prpl;
@@ -405,15 +406,16 @@ purple_prpl_send_attention(PurpleConnection *gc, const char *who, guint type_cod
 	g_return_if_fail(gc != NULL);
 	g_return_if_fail(who != NULL);
 
-	prpl = purple_find_prpl(purple_account_get_protocol_id(gc->account));
+	account = purple_connection_get_account(gc);
+	prpl = purple_find_prpl(purple_account_get_protocol_id(account));
 	send_attention = PURPLE_PLUGIN_PROTOCOL_INFO(prpl)->send_attention;
 	g_return_if_fail(send_attention != NULL);
 
 	mtime = time(NULL);
 
-	attn = purple_get_attention_type_from_code(gc->account, type_code);
+	attn = purple_get_attention_type_from_code(account, type_code);
 
-	if ((buddy = purple_find_buddy(purple_connection_get_account(gc), who)) != NULL)
+	if ((buddy = purple_find_buddy(account, who)) != NULL)
 		alias = purple_buddy_get_contact_alias(buddy);
 	else
 		alias = who;
@@ -432,7 +434,7 @@ purple_prpl_send_attention(PurpleConnection *gc, const char *who, guint type_cod
 	if (!send_attention(gc, who, type_code))
 		return;
 
-	conv = purple_conversation_new(PURPLE_CONV_TYPE_IM, gc->account, who);
+	conv = purple_conversation_new(PURPLE_CONV_TYPE_IM, account, who);
 	purple_conv_im_write(PURPLE_CONV_IM(conv), NULL, description, flags, mtime);
 
 	g_free(description);
@@ -450,7 +452,7 @@ got_attention(PurpleConnection *gc, int id, const char *who, guint type_code)
 
 	mtime = time(NULL);
 
-	attn = purple_get_attention_type_from_code(gc->account, type_code);
+	attn = purple_get_attention_type_from_code(purple_connection_get_account(gc), type_code);
 
 	/* PURPLE_MESSAGE_NOTIFY is for attention messages. */
 	flags = PURPLE_MESSAGE_SYSTEM | PURPLE_MESSAGE_NOTIFY | PURPLE_MESSAGE_RECV;
