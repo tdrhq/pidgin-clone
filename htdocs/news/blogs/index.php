28e0489d6201bb4be7a09d223ebc38d0f4448dbe
Mark Doliner <mark@kingant.net>
2008-10-30 09:11:42 +0000

    * Close two link tags * Don't list Sean as lead developer anymore.  He's just developing   copper and bronze now.

--- htdocs/news/blogs/index.php
+++ htdocs/news/blogs/index.php
@@ -20,10 +20,10 @@
 also can be viewed directly at their respective sources.
 
 <ul>
-  <li><a href="/~seanegan/blog/">Sean Egan</a> (Lead Developer)</li>
+  <li><a href="/~seanegan/blog/">Sean Egan</a></li>
   <li><a href="/~elb/blog/">Ethan Blanton</a></li>
-  <li><a href="http://coderich.net/">Richard Laager</li>
-  <li><a href="http://www.schierer.org/~luke/log">Luke Schierer</li>
+  <li><a href="http://coderich.net/">Richard Laager</a></li>
+  <li><a href="http://www.schierer.org/~luke/log">Luke Schierer</a></li>
   <li><a href="/~kstange/blog/">Kevin Stange</a> (Webmaster)</li>
   <li><a href="http://faceprint.com/">Nathan Walp</a>
 </ul>
