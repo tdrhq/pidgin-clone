3622e05b8fbe064d8e015160655da9ce5732a9bd
Casey Ho <me@caseyho.com>
2008-12-12 08:39:17 +0000

    Adding Google website optimizer code.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,18 @@
+<!-- Begin Google Website Optimizer tracking code -->
+<script>
+function utmx_section(){}function utmx(){}
+(function(){var k='1527532502',d=document,l=d.location,c=d.cookie;function f(n){
+if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.
+length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
+d.write('<sc'+'ript src="'+
+'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
++'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
++new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+'" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
+</script><script>utmx("url",'A/B');</script>
+<!-- End Google Website Optimizer tracking code -->
+
+
 <?
   // Update these to match the current page.
   $page['title'] =   "Pidgin, the universal IM client";
