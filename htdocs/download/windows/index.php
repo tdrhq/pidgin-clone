390cc9e4451bfe3cee7c7a9bb94d781726474f01
Casey Ho <me@caseyho.com>
2008-12-12 03:21:47 +0000

    adding new version of the homepage for alpha-beta testing.  featuring lightbox with screenshots and more

--- htdocs/download/windows/index.php
+++ htdocs/download/windows/index.php
@@ -19,7 +19,9 @@
 
 <div class="box_download">
 
-<p><a href="http://downloads.sourceforge.net/pidgin/pidgin-<? echo $pidgin_version; ?>.exe">
+<p><a href="http://downloads.sourceforge.net/pidgin/pidgin-<? echo $pidgin_version; ?>.exe"
+onClick="javascript:pageTracker._trackPageview('/goals/downloads/win');"
+>
 <img src="/shared/img/button.download.png" alt="[Down Arrow]" id="downbutton" />
 Download Pidgin<br />
 <span class="number"><? echo $pidgin_version; ?> for Windows</span>
