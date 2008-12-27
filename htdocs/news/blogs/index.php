ea8b10f8a4ffaa05c82ae714a24d0136d115efd0
Kevin Stange <kevin@simguy.net>
2008-12-27 06:46:19 +0000

    Add some of the blogs more recently added to the Planet, and make the text more clear about what's on this page exactly.

--- htdocs/news/blogs/index.php
+++ htdocs/news/blogs/index.php
@@ -16,15 +16,18 @@
 
 <h1>Blogs</h1>
 
-<p>While most of our developers' blogs are syndicated on our news page, they
-also can be viewed directly at their respective sources.</p>
+<p>While our developers' blogs are syndicated on our news page, they can also
+be found at their respective sources, which may permit comments or offer
+additional historical information for the more adventurous or bored.</p>
 
 <ul>
+  <li><a href="http://theflamingbanker.blogspot.com/feeds/posts/default">John Bailey</a></li>
   <li><a href="/~seanegan/blog/">Sean Egan</a></li>
   <li><a href="/~elb/blog/">Ethan Blanton</a></li>
+  <li><a href="http://gary-kramlich.livejournal.com/data/rss">Gary Kramlich</a></li>
   <li><a href="http://coderich.net/">Richard Laager</a></li>
   <li><a href="http://www.schierer.org/~luke/log">Luke Schierer</a></li>
-  <li><a href="/~kstange/blog/">Kevin Stange</a> (Webmaster)</li>
+  <li><a href="/~kstange/blog/">Kevin Stange</a></li>
   <li><a href="http://faceprint.com/">Nathan Walp</a></li>
 </ul>
 
